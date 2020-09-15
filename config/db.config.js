8f24802407e9bb62581d86165c8a003c013efc05
Paras Patidar <patidarparas13@gmail.com>
2020-09-15 12:07:59 +0530

    Files Updated

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,4 @@
+/* 
 module.exports = {
     HOST: "localhost",
     USER: "root",
@@ -11,3 +12,20 @@ module.exports = {
       idle: 10000
     }
   };
+*/
+
+module.exports = {
+        HOST: '/cloudsql/rising-study-289606:us-central1:nodejs-jwt123456',
+        DB: 'test', // Create at step 3
+        PASSWORD: 'admin@nupay',
+        USER: 'root',
+        name: 'test',
+        connector: 'mysql',
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+};
